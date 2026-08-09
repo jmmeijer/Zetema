bbedd0b00e186f5e14fc6a3064559da2f921d37c
Jesse Meijer <j.m.meijer@outlook.com>
2026-08-09 21:56:41 +0300

    Inline summary logo asset in production bundle

--- apps/web/src/views/SummaryView.vue
+++ apps/web/src/views/SummaryView.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute, useRouter } from "vue-router";
 
-import zetemaLogoSymbol from "../assets/brand/zetema-logo-symbol.png";
+import zetemaLogoSymbol from "../assets/brand/zetema-logo-symbol.png?inline";
 import LanguageSelector from "../components/LanguageSelector.vue";
 import { useInterviewStore } from "../stores/interview";
 
